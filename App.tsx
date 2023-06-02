6cbc6f41f7037c0e8812c9f61b506178970cede4
Paulo Silva <124846102+paulosilva1978@users.noreply.github.com>
2023-06-02 17:31:24 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ActivityIndicator } from 'react-native'
 import { Home } from './src/components/Home'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold })
+  return <>{fontsLoaded ? <Home /> : <ActivityIndicator />}</>
 }
